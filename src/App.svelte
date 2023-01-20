91f026f94bc8fa6eb40ed4858567908ae0088063
AlexBlacksmith <unbloody.alex@gmail.com>
2023-01-20 12:34:44 +0500

    mega menu proof of concept

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<main>
+  <div class="wrapper">
+    <nav class="nav">
+      <div class="nav-wrapper">
+        {#each arr as [item, content]}
+          <div class="nav-item-wrapper">
+            {#if item === "Fashion"}
+            <div class="nav-item">{item}</div>
+            {:else}
+            <div class="nav-item">{item}</div>
+            <div class="nav-item-content" hidden>{content}</div>
+            {/if}
+          </div>
+        {/each}
+      </div>
+    </nav>
+  </div>
+</main>
+
+
+
+<script>
+  const navItems = {
+    "Beauty" : "Beauty Content",
+    "Fashion": "Fashion Content",
+    "Gifts": "Gifts Content",
+    "Pets": "Pets Content"
+  }
+
+  const arr = Object.entries(navItems);
+
+</script>
